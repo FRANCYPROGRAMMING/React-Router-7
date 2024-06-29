7453151eb704646da7b1f2874a8d8b060c04600e
Francesca_Francyprogrammer <francescadesimone850@gmail.com>
2024-06-29 10:14:21 +0200

    Esercizio svolto

---
+++ src/GithubUser.jsx
@@ -0,0 +1,45 @@
+// components/GithubUser.js
+import React, { useState, useEffect } from 'react';
+
+const GithubUser = ({ username }) => {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [username]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>{userData.name}</h1>
+      <p>{userData.bio}</p>
+      <img src={userData.avatar_url} alt={`${userData.name}'s avatar`} width={100} />
+    </div>
+  );
+};
+
+export default GithubUser;
